395906894cd7c083cece72ff8a649fb872ec4234
Johanna Honma <johonma@hotmail.com>
2017-11-27 13:18:16 -0700

    11/27

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var questions ={
+    dumbAndDumber :{
+     name: 'Dumb and Dumber',
+     music:[ ["Ballad of Peter Pumpkinhead by Crash Test Dummies"],["Colorblind by Counting Crows","Your Song by Ewan MacGregor","War by Henry Rollins and Bone Thugs N Harmony"]
+    ]
+   
+    //  music:[   correct=["Ballad of Peter Pumpkinhead by Crash Test Dummies"],
+    //             incorrect=["Colorblind by Counting Crows","Your Song by Ewan MacGregor","War by Henry Rollins and Bone Thugs N Harmony"]
+    // ]
+}
+};
+

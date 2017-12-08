73089045a6a4258ae8af39af207a2c4873620be6
Johanna Honma <johonma@hotmail.com>
2017-12-07 21:28:03 -0700

    last changes 12-7

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,90 +5,90 @@ var questions =[
         name: 'Dumb and Dumber',
         answers:[ "Colorblind by Counting Crows","Ballad of Peter Pumpkinhead by Crash Test Dummies","Your Song by Ewan MacGregor","War by Henry Rollins and Bone Thugs N Harmony"],
         correctAnswer: "Ballad of Peter Pumpkinhead by Crash Test Dummies",
-        picture:'<center><img src="assets/images/dumb-dumber.jpg" height="20%" width="60%"></center>' ,
+        picture:'<center><img src="assets/images/dumb-dumber.jpg" height="15%" width="50%"></center>' ,
         music: '<audio src="assets/music/Crash Test Dummies - The Ballad Of Peter Pumpkin Head.mp3" autoplay="autoplay" ></audio>',
-        video: '<center> <video source src="assets/videos/Crash Test Dummies - The Ballad Of  Peter Pumpkinhead.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+        video: '<center> <video source src="assets/videos/Crash Test Dummies - The Ballad Of  Peter Pumpkinhead.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: 'X Files',
             answers: ["Walking After You by Foo Fighters","Your Song by Ewan MacGregor", 
             "Can't Take My Eyes Off Of You by Heath Ledger", "Only Lonely by Hootie and the Blowfish"],
             correctAnswer: "Walking After You by Foo Fighters",
-            picture:'<center><img src="assets/images/xfiles.jpeg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/xfiles.jpeg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Foo Fighters - Walking After You.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/X-FILES  Walking After You  MULDERSCULLY  Revival.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/X-FILES  Walking After You  MULDERSCULLY  Revival.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: 'Trainspotting',
             answers: ["Again by Janet Jackson","I'm Gonna Be (500 Miles) by The Proclaimers","Talkshow Host by Radiohead","Lust For Life by Iggy Pop"],
             correctAnswer: "Lust For Life by Iggy Pop",
-            picture:'<center><img src="assets/images/Trainspotting.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/Trainspotting.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Iggy Pop - Lust For Life.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Trainspotting - Lust for life.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Trainspotting - Lust for life.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "Romeo & Juliet",
             answers: ["Angel by Sarah McLachlan","Talkshow Host by Radiohead","She Came On by Super Deluxe","Bittersweet Symphony by The Verve"],
             correctAnswer: "Talkshow Host by Radiohead",
-            picture:'<center><img src="assets/images/romeo-e-juliet.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/romeo-e-juliet.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Romeo  Juliet 1996 -- Radiohead -- Talk Show Host.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Romeo and Juliet - Radiohead.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video><center>'
+            video: '<center><video source src="assets/videos/Romeo and Juliet - Radiohead.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video><center>'
         },
         {
             name: "Neon Genesis Evangelion",
             answers:["Colors of the Wind by Vanessa Williams","How Do I Live by Trisha Yearwood","Cruel Angel's Thesis by Yoko Takahashi","Always Love You by Whitney Houston"],
             correctAnswer: "Cruel Angel's Thesis by Yoko Takahashi",
-            picture:'<center><img src="assets/images/neon-genesis-evangelion.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/neon-genesis-evangelion.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Neon Genesis Evangelion OP - Cruel Angels Thesis by Yoko Takahashi.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Neon Genesis Evangelion OP - Cruel Angels Thesis by Yoko Takahashi.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Neon Genesis Evangelion OP - Cruel Angels Thesis by Yoko Takahashi.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "American Pie",
             answers: ["Love Shoulda Brought You Home by Toni Braxton","Accelerator by Therapy?","Colors of the Wind by Vanessa Williams","You Wanted More by Tonic"],
             correctAnswer: "You Wanted More by Tonic",
-            picture:'<center><img src="assets/images/American_pie9.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/American_pie9.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/YOU WANTED MORE by Tonic.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Tonic - You Wanted More.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Tonic - You Wanted More.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "Jerry McGuire",
             answers: ["Free Fallin by Tom Petty","Love Shoulda Brought You Home by Toni Braxton","She Came On by Super Deluxe","Bittersweet Symphony by The Verve"],
             correctAnswer: "Free Fallin by Tom Petty",
-            picture:'<center><img src="assets/images/jerry-maguire-featured.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/jerry-maguire-featured.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Tom Petty - Free Fallin.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Tom Petty - Free Fallin.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Tom Petty - Free Fallin.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "Mrs Doubtfire",
             answers: ["Save Me by Aimee Mann","If You Stay by Backstreet Boys","Dude Looks Like A Lady by Aerosmith","From the Heart by Another Level"],
             correctAnswer: "Dude Looks Like A Lady by Aerosmith",
-            picture:'<center><img src="assets/images/MrsDoubtfire_370x280.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/MrsDoubtfire_370x280.jpg"height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Aerosmith - Dude (Looks Like A Lady).mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Aerosmith - Dude (Looks Like A Lady).mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Aerosmith - Dude (Looks Like A Lady).mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "City of Angels",
             answers: ["If You Stay by Backstreet Boys","Baby I Love Your Way by Big Mountain","Uninvited by Alanis Morrissette","From the Heart by Another Level"],
             correctAnswer: "Uninvited by Alanis Morrissette",
-            picture:'<center><img src="assets/images/coa-picture.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/coa-picture.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Alanis Morissette - Uninvited.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Alanis Morissette - Uninvited Acoustic Unplugged.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Alanis Morissette - Uninvited Acoustic Unplugged.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "The Nutty Professor",
             answers:["How Do I Live by Trisha Yearwood","Touch Me, Tease Me by Case","Because You Loved Me by Celine Dion","Free Fallin by Tom Petty"],
             correctAnswer: "Touch Me, Tease Me by Case",
-            picture:'<center><img src="assets/images/the-nutty-professor-movie-featured-800x450.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/the-nutty-professor-movie-featured-800x450.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Touch Me Tease Me.mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Case ft. Mary J. Blige  Foxy Brown- Touch Me, Tease Me.mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Case ft. Mary J. Blige  Foxy Brown- Touch Me, Tease Me.mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
         {
             name: "The Beauty and the Beast",
             answers:["Dan Stevens by Evermore","Touch Me, Tease Me by Case","Because You Loved Me by Celine Dion","If I Didn't Have You by Billy Crystal and John Goodman"],
             correctAnswer: "Dan Stevens by Evermore",
-            picture:'<center><img src="assets/images/Beauty-and-the-beast.jpg" height="20%" width="60%"></center>' ,
+            picture:'<center><img src="assets/images/Beauty-and-the-beast.jpg" height="15%" width="50%"></center>' ,
             music: '<audio src="assets/music/Dan Stevens - Evermore (From Beauty and the Beast).mp3" autoplay="autoplay" ></audio>',
-            video: '<center><video source src="assets/videos/Dan Stevens - Evermore (From Beauty and the Beast).mp4" width="60%" height="20%" type="video/mp4" autoplay="autoplay"></video></center>'
+            video: '<center><video source src="assets/videos/Dan Stevens - Evermore (From Beauty and the Beast).mp4" height="15%" width="50%" type="video/mp4" autoplay="autoplay"></video></center>'
         },
 ];
 
@@ -97,7 +97,8 @@ var correct;
 var right = 0;
 var wrong = 0;
 var musicTemp;
-var wrongMusic ='<audio src="assets/music/FAIL SOUND EFFECT.mp3" autoplay="autoplay" ></audio>';
+var wrongMusic ='<audio src="assets/music/Wrong_Buzzer_Sound_Effect[Mp3Converter.net].mp3" autoplay="autoplay" ></audio>';
+var clickingSound = '<audio src="assets/music/Clock_ticking.mp3" autoplay="autoplay" ></audio>';
 var movieVideo;
 var thankYou= '<img src="assets/images/thankyou.jpg" height=50% width=100%>';
 
@@ -115,6 +116,7 @@ $("#start").click(function(){
       
 function run() {
     intervalId = setInterval(decrement, 1000);
+   
 }
     
  function decrement() {
@@ -130,8 +132,8 @@ function run() {
 
     
 function trivia(){
-       
-        
+    $('#movieMusic').append(clickingSound);
+    // $('#movieMusic').append(wrongMusic);
         if (index===(questions.length)){
             $('#msj').html("Thank you for playing!!");
             $('#right').text('Correct Answers: '+ right);
@@ -141,6 +143,7 @@ function trivia(){
             $('#movieImage').append(thankYou);
             $('.panel').empty();
             $('.button').hide();
+            $('#movieMusic').empty();
             clearTimeout(triviaTime);
             clearTimeout(checkAnswerTime);
             clearInterval(intervalId);
